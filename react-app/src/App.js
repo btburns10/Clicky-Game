aae109513698daed1aa90fb019bfc2909225a8c5
Burnsy <btburns10@gmail.com>
2018-10-16 23:37:16 -0400

    refactor components and click event handler added

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,29 +5,36 @@ import CharacterCard from "./components/CharacterCard/CharacterCard";
 import characters from "./characters.json";
 
 class App extends Component {
-  //setting initial state to characters array
+  //setting initial state
   state = {
-    characters
+    characters: characters,
+    clickedCharacters: [],
+    score: 0,
+    highScore: 0,
+    message: ""
   };
 
+  handleClick = (id) => {
+    console.log(id);
+  };
   // Map over this.state.friends and render a CharacterCard component for each character object
   render() {
     return (
-      <div>
-        <Header />
         <Wrapper>
+        <Header />
           {this.state.characters.map(character => (
             <CharacterCard
               id={character.id}
               key={character.id}
               name={character.name}
               image={character.image}
+              handleClick={this.handleClick}
             />
           ))}
         </Wrapper>
-      </div>
     );
   }
+
 }
  
  
