b3b9308117e5c62a140a4d2a7f732e7ef0edfb65
Burnsy <btburns10@gmail.com>
2018-10-17 17:00:35 -0400

    click event game logic implemented

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,14 +8,26 @@ class App extends Component {
   //setting initial state
   state = {
     characters: characters,
-    clickedCharacters: [],
+    clickedCharactersId: [],
     score: 0,
     highScore: 0,
     message: ""
   };
 
   handleClick = (id) => {
-    console.log(id);
+
+    for(let stateId of this.state.clickedCharactersId) {
+      if(stateId === id) {
+        alert("You lose!");
+        return;
+      }
+    };
+    //push click id into clickedCharactersId array state if not already clicked
+    this.setState({
+      clickedCharactersId: [...this.state.clickedCharactersId, id]
+    });
+    console.log(this.state.clickedCharactersId);
+    
   };
   // Map over this.state.friends and render a CharacterCard component for each character object
   render() {
